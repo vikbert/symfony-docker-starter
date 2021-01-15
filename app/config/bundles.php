eb1e852d2829fc933c411199561d6a08c2024105
Xun Zhou <segentor@gmail.com>
2021-01-15 21:17:57 +0100

    install oauth2-client-bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
